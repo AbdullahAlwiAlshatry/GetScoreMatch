5e3f23b3dbd8651c0f2f09c554b0b0ddbb61584c
AbdullahAlwiAlshatry <abdullah.alwi.alshatry.100@gmail.com>
2025-11-10 15:29:00 +0300

    Initial commit for C# desktop project

---
+++ GetScoreMatch/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace GetScoreMatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericM = new System.Windows.Forms.NumericUpDown();
+            this.numericD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(81, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get scores of matches";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Simplified Arabic", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(165, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Score";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(420, 24);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // numericM
+            // 
+            this.numericM.Location = new System.Drawing.Point(180, 148);
+            this.numericM.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericM.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericM.Name = "numericM";
+            this.numericM.Size = new System.Drawing.Size(120, 24);
+            this.numericM.TabIndex = 4;
+            this.numericM.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericD
+            // 
+            this.numericD.Location = new System.Drawing.Point(180, 188);
+            this.numericD.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericD.Name = "numericD";
+            this.numericD.Size = new System.Drawing.Size(120, 24);
+            this.numericD.TabIndex = 5;
+            this.numericD.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label2.Location = new System.Drawing.Point(306, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label3.Location = new System.Drawing.Point(306, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Day";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(806, 18);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(574, 311);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 359);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericD);
+            this.Controls.Add(this.numericM);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericM;
+        private System.Windows.Forms.NumericUpDown numericD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ GetScoreMatch/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace GetScoreMatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StreamReader sr;
+        Regex regex = new Regex(">(.+)<\\/p>[.+|\n+].+[\n]{4}.+\n.+\n.+>(.+)<.+\n.+\n.+>(.+)<.+\n{2}.+>(.+)<.+\n.+\n{4}.+\n{2}.+\n.+\n.+p>(.+)<");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date=" + numericM.Value + "/" + numericD.Value + "/" + DateTime.Now.Year + "#days";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(textBox1.Text);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                sr = new StreamReader(response.GetResponseStream());
+                richTextBox1.Text = sr.ReadToEnd();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Erorr.. Contact With Us !! / Enter Correct Date");
+                Application.Exit();
+            }
+
+            printDocument1.Print();
+        }
+
+
+
+
+        int i = 0;
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            var HTMLSourceCode = GetRegex(richTextBox1.Text).Skip(i);
+            if (HTMLSourceCode.Count() == 0)
+            {
+                MessageBox.Show("No Matches Were Held.. ");
+                return;
+            }
+
+            Font f = new Font("Arial", 16);
+            Font ff = new Font("Arial", 22);
+            int y = 20;
+            int yy = 20;
+            int yyy = 20;
+
+            foreach (var item in HTMLSourceCode)
+            {
+                e.Graphics.DrawString(item.Leauge, f, Brushes.Black, 540, 10 );
+                foreach (var c in item.InnerInformation)
+                {
+                    e.Graphics.DrawString(c.Week, new Font("Arial",22,FontStyle.Bold), Brushes.Black, 40, 10 );
+                    e.Graphics.DrawString(c.ScoreB, ff, Brushes.Black, 330, 30 + y);
+                    e.Graphics.DrawString(c.TeamB, f, Brushes.Black, 160, 38 + yy);
+                    e.Graphics.DrawString(c.Time, f, Brushes.DimGray, 380, 30 + y);
+                    e.Graphics.DrawString(c.TeamA, f, Brushes.Black, 530, 38 + yy);
+                    e.Graphics.DrawString(c.ScoreA, ff, Brushes.Black, 460, 30 + y);
+                    e.Graphics.DrawLine(Pens.LightGray, 0, 70 + yyy, 1000, 70 + yyy);
+                    e.Graphics.DrawImage(Image.FromFile("logo.jpg"), 13, 1115, 100, 41);
+                    y += 68;
+                    yy += 68;
+                    yyy += 68;
+                }
+                i += 1;
+
+                if (HTMLSourceCode.Count() != 1)
+                {
+                    e.HasMorePages = true;
+                    break;
+                }
+                else { i = 0; }
+            }
+
+        }
+
+        List<OuteInformation> GetRegex(string Response)
+        {
+            string html = Response;
+            List<OuteInformation> Leauges = new List<OuteInformation>();
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNodeCollection rows = doc.DocumentNode.SelectNodes("//div[contains(@class, 'matchCard') and contains(@class, 'matchesList')]");
+
+            if (rows != null)
+            {
+                foreach (var node in rows)
+                {
+
+                    var titleLeauge = Regex.Match(node.OuterHtml, @"<h2>\s*(.*?)\s*<\/h2>");
+                    string tournament = titleLeauge.Success ? titleLeauge.Groups[1].Value : "غير موجود";
+
+                    var picLeauge = Regex.Match(node.OuterHtml, "https:\\/\\/media\\.gemini\\.media\\/img\\/yallakora\\/Tourlogo\\/[^\\s\"\"']+");
+                    string pic = picLeauge.Success ? picLeauge.Groups[1].Value : "غير موجود";
+
+                    //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(textBox1.Text);
+                    //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    //if (!Directory.Exists("Pictures")){ Directory.CreateDirectory("Pictures"); }
+
+
+                    HtmlAgilityPack.HtmlDocument docChild = new HtmlAgilityPack.HtmlDocument();
+                    docChild.LoadHtml(node.OuterHtml);
+                    HtmlNodeCollection rowPartial = docChild.DocumentNode.SelectNodes("//div[contains(@class, 'allData')]");
+                    List<InnerInformation> Matches = new List<InnerInformation>();
+
+                    foreach (var c in rowPartial)
+                    {
+                        var weekLeauge = Regex.Match(node.OuterHtml, @"<div class=""date"">\s*(.*?)\s*<\/div>");
+                        string week = weekLeauge.Success ? weekLeauge.Groups[1].Value : "غير موجود";
+
+                        var teamMatches = Regex.Matches(c.OuterHtml, @"<p>\s*(.+)\s*<\/p>");
+                        List<string> teams = new List<string>();
+                        foreach (Match match in teamMatches) { teams.Add(match.Groups[1].Value); }
+
+                        var timeMatch = Regex.Match(c.OuterHtml, @"<span class=""time"">\s*(\d{1,2}:\d{2})\s*<\/span>");
+                        string matchTime = timeMatch.Success ? timeMatch.Groups[1].Value : "غير موجود";
+
+                        var scoreMatches = Regex.Matches(c.OuterHtml, "<span class=\"score\">([\\-0-9]+)<\\/span");
+                        List<string> scores = new List<string>();
+                        foreach (Match match in scoreMatches) { scores.Add(match.Groups[1].Value); }
+
+
+                        Matches.Add(new InnerInformation { TeamA = teams[0], TeamB = teams[1], ScoreA = scores[0], ScoreB = scores[1], Time = matchTime, Week = week });
+                    }
+                    Leauges.Add(new OuteInformation { Leauge = tournament, Pictures = picLeauge.Value, InnerInformation = Matches });
+                }
+                return Leauges;
+            }
+            else
+            {
+                MessageBox.Show("❌ No Matches Were Held..");
+                return Leauges = null;
+            }
+        }
+    }
+
+    internal class OuteInformation
+    {
+        public string Leauge { get; set; }
+        public string Pictures { get; set; }
+        public List<InnerInformation> InnerInformation { get; set; }
+    }
+
+    internal class InnerInformation
+    {
+        public string Week { get; set; }
+        public string Time { get; set; }
+        public string TeamA { get; set; }
+        public string TeamB { get; set; }
+        public string ScoreA { get; set; }
+        public string ScoreB { get; set; }
+    }
+}
+    
+
+//keep learning...
